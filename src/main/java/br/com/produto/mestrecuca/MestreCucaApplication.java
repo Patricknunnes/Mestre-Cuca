f079f2d429b9221297c658250908ce91f1755228
Patricknunnes <pikerox140@gmail.com>
2022-10-22 22:56:42 -0300

    ambiente configurado

---
+++ src/main/java/br/com/produto/mestrecuca/MestreCucaApplication.java
@@ -0,0 +1,13 @@
+package br.com.produto.mestrecuca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MestreCucaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MestreCucaApplication.class, args);
+    }
+
+}
